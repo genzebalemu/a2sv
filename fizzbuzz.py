aba0cf3b5e78b7a57e1c73115c57f276c0b9589b
genzebalemu <genzebalemu46@gmal.com>
2022-09-23 08:42:00 -0700

    modified fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        list=[]
+        for i in range(n):
+                list.append(str(i+1)) 
+                if (i+1)%3 == 0 and (i+1)%5==0:
+                    list[i]="FizzBuzz"
+                elif (i+1)%3==0 and (i+1%5) !=0:
+                    list[i]="Fizz"
+                elif (i+1)%5 ==0:
+                    list[i]="Buzz"
+                
+        return list
