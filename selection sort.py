bdbb155730294a85a58d80f11143f17ff86f9339
genzebalemu <genzebalemu46@gmal.com>
2022-09-24 07:41:32 -0700

    modified selection

--- selection sort.py
+++ selection sort.py
@@ -1,32 +1,4 @@
-# class Solution: 
-#     def selectionSort(self, arr,n):
-#         for i in range(n - 1):
-#             min = i
-#             for j in range(i + 1, n):
-#                 if(arr[j] < arr[min]):
-#                     min = j
-#             arr[i], arr[min] = arr[min], arr[i] 
-#User function Template for python3
-
 class Solution: 
-    # def select(self, arr, i):
-    #     n = len(arr)
-    #     for i in range(n - 1):
-    #         min = i
-    #         for j in range(i + 1, n):
-    #             if(arr[j] < arr[min]):
-    #                 min = j
-    #         arr[i], arr[min] = arr[min], arr[i]
-        # n= len(arr)
-        # for i in range(n-1):
-        #     for j in range(n-1):
-        #         if arr[j] < arr[j-1]:
-        #             arr[j-1],arr[j]=arr[j],arr[j-1]
-          
-        #         # return arr
-            
-        # # code here 
-    
     def selectionSort(self, arr,n):
         for i in range(n - 1):
             min = i
