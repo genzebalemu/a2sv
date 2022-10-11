fe6206ff6126da7bcbe95bf06c82108e76c147d2
genzebalemu <genzebalemu46@gmal.com>
2022-10-11 08:34:31 -0700

    longest substrig

---
+++ sliding windows/longest substring  without reapet char.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        hash={}
+        max_len = 0
+        start_idx = 0
+        for i in range(0, len(s)):
+            if s[i] in hash:
+                start_idx = max(start_idx, hash[s[i]] + 1)
+            max_len = max(max_len, i-start_idx + 1)   
+            hash[s[i]] = i
+        return max_len
