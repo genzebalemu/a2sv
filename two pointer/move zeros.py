a107b3a21af28b5c6f21a94cb769daf8dae9c72d
genzebalemu <genzebalemu46@gmal.com>
2022-10-13 07:22:15 -0700

    move zeros

--- two pointer/move zeros.py
+++ two pointer/move zeros.py
@@ -1,9 +1,19 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        j = 0
+        l=0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[j], nums[i] = nums[i], nums[j] 
-                j += 1
+            if nums[i]!=0:
+                nums[l] =nums[i]
+                l +=1
+        for j in range(l,len(nums)):
+            nums[j]=0    
         return nums 
+        
+#         j = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[j], nums[i] = nums[i], nums[j] 
+#                 j += 1
+#         return nums 
                         
+             
