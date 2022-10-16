65aee27a01873df2240723c9ef09d66bf5f4486c
genzebalemu <genzebalemu46@gmal.com>
2022-10-15 23:22:12 -0700

    fruits into basket

--- sliding windows/maximum conscetive ones.py
+++ sliding windows/maximum conscetive ones.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def longestOnes(self, nums, k):
+
         l=ones=zero=0
         for r in range(len(nums)):
             if nums[r] ==0:
@@ -10,3 +11,16 @@ class Solution(object):
                 l +=1
             ones = max(ones,r-l+1)
         return ones
+
+        # j=0
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         k -=1
+        #     while k < 0:
+        #         if nums[j]==0:
+        #             k +=1
+        #         j +=1
+        #     res= max(res,i-j+1)
+        # return res
+
+    
