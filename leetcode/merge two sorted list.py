88c14ad52e3c64393bef8bebd7ddeb02edf0a2c4
genzebalemu <genzebalemu46@gmal.com>
2022-10-24 03:33:00 -0700

    merge two sorted

--- leetcode/merge two sorted list.py
+++ leetcode/merge two sorted list.py
@@ -1,33 +1,17 @@
-# Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        last=ListNode(20)
-        tail=last
-        # if list1==[] and list2==[]:
-        #     return []
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next=list1
-                list1=list1.next
+        head_ptr = temp_ptr = ListNode()
+        while list1 or list2:
+            if list1 and (not list2 or list1.val <= list2.val):
+                temp_ptr.next = ListNode(list1.val)
+                list1 = list1.next
             else:
-                tail.next=list2
-                list2=list2.next
-            tail=tail.next
-        if not list1:
-            tail.nxet=list2
-            list2=list2.next
-        else:
-            tail.next=list1
-            list1=list1.next
-        # while list1:
-        #     tail.next=list1
-        #     list1=list1.next
-        # while list2:
-        #     tail.next=list2
-        #     list2=list2.next
-        return tail.next
+                temp_ptr.next = ListNode(list2.val)
+                list2 = list2.next
+            temp_ptr = temp_ptr.next
+        return head_ptr.next
             
